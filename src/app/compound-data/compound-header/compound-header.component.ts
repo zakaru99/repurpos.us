f2921ca728d779bbc439963d050f88ec1d074493
Sherman9819 <csherman@scripps.edu>
2023-03-13 16:41:55 -0700

    added feature to download QC files

--- src/app/compound-data/compound-header/compound-header.component.ts
+++ src/app/compound-data/compound-header/compound-header.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Location } from '@angular/common';
 import { CompoundService } from '../../_services/index';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 import { Compound } from '../../_models';
+declare var $ : any
 
 @Component({
   selector: 'app-compound-header',
@@ -93,4 +95,50 @@ export class CompoundHeaderComponent implements OnInit {
     this.getNumAliases();
   }
 
+
+
+
+
+  clickedQC(vendor_ikey){
+
+    function saveAs(uri, filename) {
+      var link = document.createElement('a');
+      if (typeof link.download === 'string') {
+          document.body.appendChild(link); // Firefox requires the link to be in the body
+          link.download = filename;
+          link.href = uri;
+          link.click();
+          document.body.removeChild(link); // remove the link when done
+      } else {
+          location.replace(uri);
+      }
+  }
+
+    $.ajax({
+      type: "POST",
+      async: false,
+      url: "https://reframedb.org/php/getQCdownloadID.php",
+      data: {ikey: vendor_ikey},
+      dataType:'JSON', 
+      success: function(response){
+          var fileData = response.data;
+          if(fileData !== undefined)
+          {
+            for (var i=0; i<fileData.length; i++)
+            {
+              //console.log('downloading file id:' + fileData[i].upload_id + ' name: ' + fileData[i].file_name);
+              var filepath = '../../assets/QC_files/' + fileData[i].upload_id + fileData[i].extension;
+              //var file_name = fileData[i].file_name.substring(0, fileData[i].file_name.length-4) +  fileData[i].extension;
+              var file_name = fileData[i].file_name.split('.')[0];
+              saveAs(filepath, file_name);
+            }
+          }
+          else
+          {
+            alert("sorry, QC data for this compound is not yet available");
+          }
+        }
+    });
+
+}
 }
