1ff81a697e1f854a25b6501f67219e9e151080cc
Sherman9819 <csherman@scripps.edu>
2025-11-25 10:09:47 -0800

    implement contact-us

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
-export class ContactUsComponent implements OnInit {
+export class ContactUsComponent {
 
-  constructor() { }
+  isSubmitting = false;
+  toastMessage = "";
+  toastType = "";
 
-  ngOnInit() {
+  formData = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit(form: any) {
+    if (!form.valid) return;
+
+    this.isSubmitting = true;
+
+    this.http.post('/api/contact', this.formData).subscribe({
+      next: () => {
+        this.toastMessage = "Message sent successfully!";
+        this.toastType = "success";
+        form.resetForm();
+      },
+      error: () => {
+        this.toastMessage = "Failed to send message.";
+        this.toastType = "error";
+      },
+      complete: () => {
+        this.isSubmitting = false;
+        setTimeout(() => (this.toastMessage = ''), 3000);
+      }
+    });
   }
-
 }
