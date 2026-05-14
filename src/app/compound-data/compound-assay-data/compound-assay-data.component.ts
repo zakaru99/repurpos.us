54b443529a565b37537d67c91dcaf615c9a6c793
Sherman9819 <csherman@scripps.edu>
2026-05-14 14:31:43 -0700

    sortable column headers on the compound assay table

--- src/app/compound-data/compound-assay-data/compound-assay-data.component.ts
+++ src/app/compound-data/compound-assay-data/compound-assay-data.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-
 import { AssayData } from '../../_models/index';
 import { CompoundService, ColorPaletteService } from '../../_services/index';
 
@@ -8,38 +7,66 @@ import { CompoundService, ColorPaletteService } from '../../_services/index';
   templateUrl: './compound-assay-data.component.html',
   styleUrls: ['./compound-assay-data.component.scss']
 })
-
+ 
 export class CompoundAssayDataComponent implements OnInit {
   assayData: Array<AssayData> = [];
   assayMin: number;
-  // assay_type: string;
-  // indic: string[] = ["Zika", "Malaria", "M. tuberculosis", "Helminth: anti-Wolbachia", "Cryptosporidium", '']
 
+  // Sorting
+  sortColumn: string = '';
+  sortDirection: 'asc' | 'desc' = 'asc';
 
-  constructor(private cmpdSvc: CompoundService, private colorSvc: ColorPaletteService) {
-
-    this.cmpdSvc.assaysState.subscribe((assays: AssayData[]) => {
-      this.assayData = assays.sort((a: any, b: any) => a.ac50 - b.ac50);
-
-      this.assayMin = Math.min(...assays.filter(d => d.ac50).map((d: any) => d.ac50));
-
-    })
-
-  }
-
-  ngOnInit() {
-    // let rand = Math.floor(Math.random() * 6);
-    // this.assay_type = this.indic[rand];
-  }
-
-  activity_text = 'Show Data'
+  // Toggle activity
+  activity_text = 'Show Data';
   hide_activity: boolean = true;
-  toggle_activity_data(){
-    this.hide_activity = !this.hide_activity; 
-    if(this.hide_activity == true){
-      this.activity_text = 'Show Data'
-    }else{
-      this.activity_text = 'Hide Data'
+
+  displayedColumns: string[] = [
+    'id',
+    'title',
+    'indication',
+    'activityType',
+    'measurement',
+    'source',
+    'vendor',
+    'vendorId'
+  ];
+
+  constructor(private cmpdSvc: CompoundService, public colorSvc: ColorPaletteService) {
+    this.cmpdSvc.assaysState.subscribe((assays: AssayData[]) => {
+      this.assayData = assays;
+      this.assayMin = Math.min(...assays.filter(d => d.ac50).map((d: any) => d.ac50));
+    });
+  }
+
+  ngOnInit() {}
+
+  toggle_activity_data() {
+    this.hide_activity = !this.hide_activity;
+    this.activity_text = this.hide_activity ? 'Show Data' : 'Hide Data';
+  }
+
+  // Sorting function
+  sortData(column: string) {
+    if (this.sortColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 'asc';
     }
+
+    this.assayData.sort((a: any, b: any) => {
+      let valA = a[column];
+      let valB = b[column];
+
+      // Handle numbers
+      if (!isNaN(valA) && !isNaN(valB)) {
+        return this.sortDirection === 'asc' ? valA - valB : valB - valA;
+      }
+
+      // Handle strings
+      valA = valA ? valA.toString().toLowerCase() : '';
+      valB = valB ? valB.toString().toLowerCase() : '';
+      return this.sortDirection === 'asc' ? (valA > valB ? 1 : -1) : (valA < valB ? 1 : -1);
+    });
   }
 }
