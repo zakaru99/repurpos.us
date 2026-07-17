95013c21a3f0ca82e1f78c336e593f5dc3cd2fc8
Sherman9819 <csherman@scripps.edu>
2026-07-17 10:23:21 -0700

    update api uril path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   site_name: "ReframeDB",
   description: "ReframeDB: an open and extendable drug repurposing database",
-  api_url: 'https://reframedb.org/api',
+  api_url: '/api',
   host_url: 'https://reframedb.org/',
   url: 'https://reframedb.org/',
   googleAnalyticsKey: 'UA-115462080-1'
